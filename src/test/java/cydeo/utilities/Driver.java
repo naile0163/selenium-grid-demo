3ed4709ca53662fc14eb5f38a201f2a68eff4b11
mehmetCt <mehmet@cybertekschool.com>
2024-12-26 12:41:16 -0500

    Grid IP is added

--- src/test/java/cydeo/utilities/Driver.java
+++ src/test/java/cydeo/utilities/Driver.java
@@ -48,7 +48,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.210.88.112";
+                        String gridAddress = "100.29.38.166";
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         ChromeOptions chromeOptions = new ChromeOptions();
                         chromeOptions.addArguments("--start-maximized");
